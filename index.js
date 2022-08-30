714cef82c8c6e181719651267493b932d59768c8
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-08-30 17:11:30 +0600

    added static file in the index file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
     console.log(`Server listen at: ${PORT}`)
