4e98907a01a88ecf1d3f142772497ec9c2810c34
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-08-31 12:30:18 +0600

    feat: room name and users content made dynamic and added to the DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
 const chatForm = document.getElementById('chat-form')
 const chatMesssages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true,
-  });
+});
 
 
 
@@ -12,6 +14,13 @@ const socket = io()
 
 // Join chatroom
 socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
+
 
 // message from server
 socket.on('message', message => {
@@ -36,7 +45,7 @@ chatForm.addEventListener('submit', (e) => {
 })
 
 
-function outPutMessage(msg){
+function outPutMessage(msg) {
     const div = document.createElement('div');
 
     div.classList.add('message');
@@ -47,4 +56,19 @@ function outPutMessage(msg){
     </p>`
 
     document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = '';
+    users.forEach((user) => {
+        const li = document.createElement('li');
+        li.innerText = user.username;
+        userList.appendChild(li);
+    });
 }
